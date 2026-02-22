8820bc15089e7ad2c4ce9bddd3869ec003fc1193
Phong Dau <phongdau119474@gmail.com>
2026-02-22 10:27:43 -0600

    XOR Strings

---
+++ stringsXOR/Solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static String stringsXOR(String s, String t) {
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == t.charAt(i))
+                res.append("0");
+            else
+                res.append("1");
+        }
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String s, t;
+        Scanner in = new Scanner(System.in);
+        s = in.nextLine();
+        t = in.nextLine();
+        System.out.println(stringsXOR(s, t));
+        in.close();
+
+    }
+
+}
+
+
+
